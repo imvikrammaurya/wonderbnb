24324f25c2bd5f1e9380d54cfd52f237aff1d2f9
Vikram Maurya <imvikrammaurya@outlook.com>
2025-10-05 15:30:18 +0530

    some changes in filter part and add functionality in delete review that only owner of the review can delete there review

--- routes/review.js
+++ routes/review.js
@@ -15,7 +15,7 @@ const reviewController =  require("../controllers/reviews.js")
 router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
 //Delete Review Route
-router.delete("/:reviewId",isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview));
+router.delete("/:reviewId", wrapAsync(reviewController.destroyReview));
 
 
 module.exports = router;
